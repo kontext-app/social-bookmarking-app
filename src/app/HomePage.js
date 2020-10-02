41e728f9a75ceba70f3ff205fe669e2db02fbfc1
seichris <cerujurasa@gmail.com>
2020-10-02 18:52:17 +0200

    new branch

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function HomePage() {
   return (
     <div className="container mx-auto">
-      <div className="text-xl">Welcome to Kontext!</div>
+      <div className="text-xl">Welcome to Kontext!!</div>
     </div>
   );
 }
