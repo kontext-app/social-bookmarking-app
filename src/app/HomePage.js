19227e145968face4627fb4e706130073efc95a7
seichris <cerujurasa@gmail.com>
2020-10-09 14:37:40 +0200

    design homepage css

--- src/app/HomePage.js
+++ src/app/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Post } from './components/Post.js';
 
 export function HomePage() {
   return (
-    <div className="container mx-auto">
-      <div className="text-xl">Welcome to Kontext!!</div>
+    <div className="bg-gray-200">
+      <div className="container mx-auto">
+        <div class="flex justify-center ">
+          <div class="text-gray-700 text-center px-4 py-2 m-2">
+            <Post />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
