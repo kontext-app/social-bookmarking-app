910c44d4ace5d121f6735b22cfad0115af58576b
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-11-06 16:03:51 +0100

    Enable eslint typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
-  extends: ["react-app"],
-  plugins: ["prettier"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
   },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
 };
