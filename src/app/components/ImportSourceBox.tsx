bfab1eb891ad176bb711ae86a365fff8f16e0ac6
seichris <cerujurasa@gmail.com>
2021-02-01 13:55:51 +0100

    ux update add-bookmark - fix link disable

--- src/app/components/ImportSourceBox.tsx
+++ src/app/components/ImportSourceBox.tsx
@@ -8,17 +8,14 @@ type Props = {
   link: string;
 };
 
-//function DisableClick { (event) => event.preventDefault() };
-
 export function ImportSourceBox(props: Props) {
   return (
     <div className="my-5 px-5 w-1/4 overflow-hidden sm:w-1/3 md:w-1/2 lg:w-1/4 xl:w-1/4">
       <Link
-        to={props.link}
+        to={props.enabled ? props.link : "/add-bookmark"}
         className={`${
           props.enabled == false && 'cursor-default'
           } hidden md:flex justify-center`}
-        /*onClick={ props.enabled == false && {DisableClick} }*/
         >
         <div className="flex flex-col">
           <h1 className="pb-4">{props.title}</h1>
